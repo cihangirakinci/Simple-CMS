34429a6193e0f0a2522c9ece05c603e48b76d105
cihangirakinci <akincicihangir@gmail.com>
2020-02-10 23:43:57 -0500

    updated

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,20 @@
 class DemoController < ApplicationController
+
+  layout false
+
   def index
+    render('index')
+  end
+
+  def hello
+    render('hello')
+  end
+
+  def other_hello
+    redirect_to(:action => 'index')
+  end
+
+  def lynda
+    redirect_to('https://www.lynda.com/')
   end
 end
