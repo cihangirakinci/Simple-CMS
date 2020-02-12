20cbde13946d4fa99a6920263333684683eb1688
cihangirakinci <akincicihangir@gmail.com>
2020-02-12 09:19:16 -0500

    new migration and changed column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_042247) do
+ActiveRecord::Schema.define(version: 2020_02_12_004333) do
+
+  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "first_name", limit: 25
+    t.string "last_name", limit: 50
+    t.string "email", limit: 100, default: "", null: false
+    t.string "username", limit: 25
+    t.string "hashed_password", limit: 40
+    t.datetime "created_at", null: false
+    t.datetime "update_at"
+    t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_admin_users_on_username"
+  end
 
   create_table "pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "subject_id"
@@ -41,16 +53,6 @@ ActiveRecord::Schema.define(version: 2020_02_11_042247) do
     t.integer "position"
     t.boolean "visible", default: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "first_name", limit: 25
-    t.string "last_name", limit: 50
-    t.string "email", default: "", null: false
-    t.string "password", limit: 40
-    t.datetime "created_at", null: false
-    t.datetime "update_at"
     t.datetime "updated_at", null: false
   end
 
